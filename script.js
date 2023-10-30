0247d23c7c75e4c315fc9850ca2b1ab2048bbb35
Yieison Lizarazo <76229560+Yieison@users.noreply.github.com>
2023-10-30 16:41:11 +0000

    Script Funcional

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-const apiUrl = 'https://siaweb-nodejs.carlos-reneren7.repl.co/productos';
+document.addEventListener("DOMContentLoaded", function() {
+    // Elemento donde se mostrarán los datos
+    var dataBody = document.getElementById("data-body");
 
-const dataTable = document.getElementById('data-table');
-const dataBody = document.getElementById('data-body');
+    // Realizar la solicitud a la URL
+    fetch("https://siaweb-nodejs.carlos-reneren7.repl.co/productos")
+        .then(function(response) {
+            return response.json(); // Convertir la respuesta a JSON
+        })
+        .then(function(data) {
+            // Procesar los datos y agregarlos a la tabla
+            data.forEach(function(product, index) {
+                var row = document.createElement("tr");
 
-function fetchData(page) {
+                var numberCell = document.createElement("td");
+                numberCell.textContent = index + 1;
 
-    // Hacer una solicitud GET a la API
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('La solicitud no fue exitosa');
-        }
-        return response.json(); // Convierte la respuesta en formato JSON
-      })
-      .then(data => {
-        // Limpiar cualquier contenido previo en el cuerpo de la tabla
-        dataBody.innerHTML = '';
+                var nameCell = document.createElement("td");
+                nameCell.textContent = product.nombre;
 
-        // Mostrar los datos de la API en la página actual
-        for (let i = startIndex; i < endIndex && i < data.length; i++) {
-          const item = data[i];
-          const row = document.createElement('tr');
-          const cell1 = document.createElement('td');
-          const cell2 = document.createElement('td');
-          const cell3 = document.createElement('td');
-          const cell4 = document.createElement('td');
+                var priceCell = document.createElement("td");
+                priceCell.textContent = product.precio;
 
-          cell1.textContent = i + 1; // Número de producto
-          cell2.textContent = item.nombre;
-          cell3.textContent = item.precio;
-          cell4.textContent = item.descripcion;
+                var descriptionCell = document.createElement("td");
+                descriptionCell.textContent = product.descripcion;
 
-         
-          row.appendChild(cell1);
-          row.appendChild(cell2);
-          row.appendChild(cell3);
-          row.appendChild(cell4);
+                row.appendChild(numberCell);
+                row.appendChild(nameCell);
+                row.appendChild(priceCell);
+                row.appendChild(descriptionCell);
 
-          dataBody.appendChild(row);
-        }
-
-      })
-      .catch(error => {
-        // Manejar errores
-        console.error('Error:', error);
-      });
-};
+                dataBody.appendChild(row);
+            });
+        })
+        .catch(function(error) {
+            console.error("Error al obtener los datos:", error);
+        });
+});
