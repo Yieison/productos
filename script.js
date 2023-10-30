07f6f72242d5df8b490260906d38899f58a28269
Yieison Lizarazo <76229560+Yieison@users.noreply.github.com>
2023-10-30 19:36:07 +0000

    Se añade todo en un solo script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*OBTENER PRODUCTOS*/
 document.addEventListener("DOMContentLoaded", function() {
     var dataBody = document.getElementById("data-body");
 
@@ -46,3 +47,34 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error("Error al obtener los datos:", error);
         });
 });
+
+/*AÑADIR PRODUCTOS*/
+document.getElementById('miBoton').addEventListener('click', function() {
+    var nombre = document.getElementById('nombre').value;
+    var precio = document.getElementById('precio').value;
+    var descripcion = document.getElementById('descripcion').value;
+
+    fetch('https://siaweb-nodejs.carlos-reneren7.repl.co/productos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            precio: precio,
+            descripcion: descripcion
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
+
+/*CANCELAR*/
+document.getElementById('botoncancelar').onclick = function() {
+    window.location.href = '../index.html';
+}
